c00363dfd90bf9e0f090ecbb8470f287d19c5487
Mannan <abdul.mannan@mavs.uta.edu>
2023-02-24 22:57:00 -0500

    test

--- src/main/java/com/preservr/agent/AgentApplication.java
+++ src/main/java/com/preservr/agent/AgentApplication.java
@@ -8,7 +8,7 @@ public class AgentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgentApplication.class, args);
-		System.out.println("hello from Spring Boot");
+		System.out.println("Testing");
 	}
 
 }
